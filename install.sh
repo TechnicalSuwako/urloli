09b5f890398504866cb3e6e96af53baa4d283841
テクニカル諏訪子 <renraku@076.ne.jp>
2023-02-07 10:32:33 +0900

    ごめん

--- install.sh
+++ install.sh
@@ -27,9 +27,10 @@ if [ "$unamestr" = 'Linux' ]; then
   chmod +x /etc/init.d/urloli
   /etc/init.d/urloli start
 elif [ "$unamestr" = 'OpenBSD' ]; then
-  pkg_add certbot
   cat /etc/acme-client.conf src/openbsd/etc/acme-client.conf > /etc/acme-client.conf
   cat /etc/httpd.conf srv/openbsd/etc/httpd.conf > /etc/httpd.conf
+  rcctl restart httpd
+  acme-client -v urlo.li
   cp srv/openbsd/etc/rc.d/urloli /etc/rc.d
   chmod +x /etc/rc.d/urloli
   rcctl start urloli
